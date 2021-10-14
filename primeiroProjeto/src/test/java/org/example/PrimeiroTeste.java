a8fda9f575bed3fe86fdcc016ab2756e1e5cb39c
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-10-14 18:36:54 -0300

    up

--- primeiroProjeto/src/test/java/org/example/PrimeiroTeste.java
+++ primeiroProjeto/src/test/java/org/example/PrimeiroTeste.java
@@ -6,14 +6,34 @@ import org.junit.Test;
 public class PrimeiroTeste {
 
     @Test
-    public void estruturaDeUmTeste () {
-    //cenário
-    int num1 = 10, num2 = 5;
+    public void deveSomarDoisNumeros() {
+        //cenário
+        int num1 = 10, num2 = 5;
 
-    //execução
-    int resultado = num1 + num2;
+        //execução
+        int resultado = num1 + num2;
 
-    //verificações
+        //verificações
         Assert.assertEquals(15, resultado);
+
+        @Test
+        public void naoDeveSomarNumerosNegativos() {
+            //cenário
+            Calculadora calculadora = new Calculadora();
+            int numA = -10;
+            int numB = 5;
+        }
     }
+
+
+    class Calculadora {
+
+        int somar(int num1, int num2) {
+            return num1 + num1;
+        }
+
+
+
+    }
+
 }
