105ad18cc0b1af20b4360b41b9a7f7ba4dd74783
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-10-14 19:55:07 -0300

    up

--- primeiroProjeto/src/test/java/org/example/PrimeiroTeste.java
+++ primeiroProjeto/src/test/java/org/example/PrimeiroTeste.java
@@ -8,20 +8,16 @@ public class PrimeiroTeste {
     @Test
     public void deveSomarDoisNumeros() {
         //cenário
+        Calculadora calculadora = new Calculadora();
         int num1 = 10, num2 = 5;
 
         //execução
-        int resultado = num1 + num2;
+        int resultado = calculadora.somar(10, 5);
 
         //verificações
         Assert.assertEquals(15, resultado);
 
-        @Test
-        public void naoDeveSomarNumerosNegativos() {
-            //cenário
-            Calculadora calculadora = new Calculadora();
-            int numA = -10;
-            int numB = 5;
+
         }
     }
 
@@ -29,9 +25,23 @@ public class PrimeiroTeste {
     class Calculadora {
 
         int somar(int num1, int num2) {
+            if (num1 < 0 || num2 < 0) {
+                throw new RuntimeException("Não é permitido números negativos");
+            }
             return num1 + num1;
         }
 
+        int multiplicar(int num1, int num2) {
+            return num1 * num2;
+        }
+
+        double dividir(double doub1, double doub2) {
+            return doub1 / doub2;
+        }
+
+        int subtrair(int num1, int num2) {
+            return num1 - num2;
+        }
 
 
     }
