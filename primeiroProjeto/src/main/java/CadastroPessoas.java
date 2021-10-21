15d93fe19ba3e9038b3b03743a91c7c8be71a665
Larissa Fraga Melo <84105505+lariframelo@users.noreply.github.com>
2021-10-21 15:04:54 -0300

    up

---
+++ primeiroProjeto/src/main/java/CadastroPessoas.java
@@ -0,0 +1,12 @@
+import Pessoa.Pessoa;
+
+import java.util.List;
+
+public class CadastroPessoas {
+
+    private List<Pessoa> pessoas;
+
+    public List<Pessoa> getPessoas() {
+        return this.pessoas;
+    }
+}
---
+++ primeiroProjeto/src/test/java/CadastroPessoasTeste.java
@@ -0,0 +1,21 @@
+import com.sun.xml.internal.ws.policy.AssertionSet;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CadastroPessoasTeste {
+
+    @Test
+    public void deveCriarOCadastroPessoas(){
+        //cenário
+        CadastroPessoas cadastro = new CadastroPessoas();
+
+
+        //verificação
+
+        Assertions.assertThat(cadastro).();
+    }
+}
